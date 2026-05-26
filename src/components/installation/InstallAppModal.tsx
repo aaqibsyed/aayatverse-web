c19ecfb586bf93ebb4e72eabf7f107360a4714e3
Aaqib <syedaaqib89@gmail.com>
2026-05-27 00:24:47 +0530

    fix/Making AppModal scrollable

--- src/components/installation/InstallAppModal.tsx
+++ src/components/installation/InstallAppModal.tsx
@@ -69,7 +69,7 @@ export default function InstallAppModal({
           p-0
         "
       >
-        <div className="flex h-full flex-col">
+        <div className="flex h-full min-h-0 flex-col">
           {/* Header */}
           <DialogHeader
             className="
@@ -102,7 +102,7 @@ export default function InstallAppModal({
           <div
             className="
               flex-1
-
+              min-h-0
               overflow-y-auto
             "
           >
@@ -255,9 +255,6 @@ export default function InstallAppModal({
                   flex
                   items-center
                   justify-between
-                  sticky
-                  bottom-0
-                  shrink-0
                 "
               >
                 <button
